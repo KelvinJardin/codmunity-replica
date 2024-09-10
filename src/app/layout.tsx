9d98de32a5ee03bbf4a2ba572d36a2e08c7c838e
Jack <103641447+lambo94@users.noreply.github.com>
2024-09-10 12:01:24 +0100

    Began work on styling, installed clsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextAuthProvider } from "@/components/Auth";
+import AppBar from "@/components/Layout/AppBar"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,8 @@ export default function RootLayout({
           <NextAuthProvider>
                 {children}
           </NextAuthProvider>
+          {/* testing for now */}
+          <AppBar />
         </body>
       </html>
   );
--- src/components/Layout/AppBar.tsx
+++ src/components/Layout/AppBar.tsx
@@ -1,2 +1,41 @@
 // App bar at the bottom of page
 // usages: everywhere
+'use client';
+
+import Link from "next/link";
+import LinkIcon from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+import nav from "react";
+
+//Probably could have this populate from the DB but its static so...
+const links = [
+    { name: 'Home', href: '/home', icon: 'HomeIcon' },
+    { name: 'Meta Hub', href: '/meta', icon: 'MetaIcon' },
+    { name: 'Creators', href: '/creators', icon: 'CreatorsIcon' },
+    { name: 'TTK Tool', href: '/ttk', icon: 'TtkToolIcon' },
+    { name: 'Hub', href: '/hub', icon: 'HubIcon' },
+];
+
+export default function AppBar() {
+    const pathName = usePathname();
+
+    return (
+        <nav className="AppBar">
+            {links.map((link) => {
+                return (
+                    <Link key={link.name} href={link.href}
+                          className={clsx(
+                              "flex gap-2 text-gray-400 hover:text-green-600 bg-black p-3 text-xs",
+                              {
+                                  "text-green-600" : pathName === link.href,
+                              },
+                          )}
+                    >
+                        <p className="hidden md:block">{link.name}</p>
+                    </Link>
+                );
+            })}
+        </ul>
+    );
+}
