cd273ef1d6a70b15a52167494c8679c40ddd124f
Jack <103641447+lambo94@users.noreply.github.com>
2024-09-11 11:33:35 +0100

    App bar buttons actually use the router now

--- src/components/Layout/AppBar.tsx
+++ src/components/Layout/AppBar.tsx
@@ -5,30 +5,31 @@ import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
 import EqualizerRoundedIcon from '@mui/icons-material/EqualizerRounded';
 import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
 import CarpenterRoundedIcon from '@mui/icons-material/CarpenterRounded';
-import {BottomNavigation, BottomNavigationAction} from "@mui/material";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import Paper from '@mui/material/Paper';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from "react";
 import clsx from "clsx";
 
 //Probably could have this populate from the DB but its static so...
 const links = [
     { name: 'Home', href: '/', icon: HomeRoundedIcon },
-    { name: 'Meta Hub', href: '/meta', icon: CarpenterRoundedIcon }, // There's no icon in MUI for a gun :P
-    { name: 'Creators', href: '/creators', icon: GroupsRoundedIcon },
-    { name: 'TTK Tool', href: '/ttk', icon: EqualizerRoundedIcon },
-    { name: 'Hub', href: '/hub', icon: GridViewRoundedIcon },
+    { name: 'Meta Hub', href: '/MetaHub', icon: CarpenterRoundedIcon }, // There's no icon in MUI for a gun :P
+    { name: 'Creators', href: '/Creators', icon: GroupsRoundedIcon },
+    { name: 'TTK Tool', href: '/TTKTools', icon: EqualizerRoundedIcon },
+    { name: 'Hub', href: '/Hub', icon: GridViewRoundedIcon },
 ];
 
 export default function AppBar() {
     const pathName = usePathname();
+    const router = useRouter();
 
 // Paper is pretty handy here for z-indexing I think, there might be a better way to do this though
     return (
         <>
             <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
                 <BottomNavigation
-                    className="bg-black text-lim"
+                    className="bg-black"
                     showLabels
                     value={pathName}
                 >
@@ -40,6 +41,9 @@ export default function AppBar() {
                             label={link.name}
                             icon={<LinkIcon />}
                             className={clsx("text-gray-400", {"text-lime-500" : pathName === link.href})}
+                            onClick={(e) => {
+                                router.push(link.href);
+                            }}
                         />
                     })}
                 </BottomNavigation>
